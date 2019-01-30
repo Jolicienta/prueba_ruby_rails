f6d59fecde800848d4fb3fda6f736c3379e4ca05
Jolicienta <jolicienta.rp@gmail.com>
2019-01-30 16:16:35 -0300

    Prueba de Ruby on Rails

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,14 @@ Rails.application.routes.draw do
   #ruta '/todos' que apunte al método todos#index
   get 'todos', to: 'todos#index'
   get 'todos/new', to: 'todos#new'
+  post 'todos', to: 'todos#create'
+  get 'todos/:id', to: 'todos#show', as: 'todo'
+  get 'todos/:id/edit', to: 'todos#edit', as: 'edit'
+  patch 'todos/:id', to: 'todos#update'
+  delete 'todos/:id', to: 'todos#destroy', as: 'delete'
+  patch 'todos/:id/complete', to: 'todos#complete', as: 'completed'
+  get 'todos/list', to: 'todos#list'
 
   root 'todos#index'
-
-
 
 end
